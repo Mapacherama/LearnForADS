882b3c51646565d28424087f6b7f34fcf6c8a553
jerome tesselaar <jerome.tesselaar@hva.nl>
2021-05-20 17:27:22 +0200

    The retry of graphs works

--- Code in Java/Graphs retry/src/com/Mapacherama/Graph.java
+++ Code in Java/Graphs retry/src/com/Mapacherama/Graph.java
@@ -36,10 +36,58 @@ public class Graph<T> {
 
     // This function gives the count of vertices
     public void getVertexCount() {
+        System.out.println("The graph has " + map.keySet().size()
+                + " vertex");
+    }
+
+    // This function gives the count of vertices
+    public void getEdgesCount(boolean bidirection) {
         int count = 0;
         for (T v : map.keySet()
-             ) {
+        ) {
             count += map.get(v).size();
         }
+        if (bidirection) {
+            count = count / 2;
+        }
+
+        System.out.println("The graph has " + count + " edges.");
+    }
+
+    // This function gives whether a vertex is present or not.
+    public void hasVertex(T s) {
+        if (map.containsKey(s)) {
+            System.out.println("The graph contains " + s + " as a vertex.");
+        } else {
+
+            System.out.println("The graph does not contain " + s + " as a vertex");
+        }
+    }
+
+    // This function gives whether an edge is present or not.
+    public void hasEdge(T s, T d) {
+        if (map.get(s).contains(d)) {
+            System.out.println("The graph has an edge between "
+                    + s + " and " + d + ".");
+        } else {
+            System.out.println("The graph has no edge between "
+                    + s + " and " + d + ".");
+        }
+    }
+
+    // Prints the adjacency list of each vertex.
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+
+        for (T v : map.keySet()) {
+            builder.append(v.toString() + ": ");
+            for (T w : map.get(v)) {
+                builder.append(w.toString() + " ");
+            }
+            builder.append("\n");
+        }
+
+        return (builder.toString());
     }
 }
--- Code in Java/Graphs retry/src/com/Mapacherama/Main.java
+++ Code in Java/Graphs retry/src/com/Mapacherama/Main.java
@@ -2,7 +2,37 @@ package com.Mapacherama;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String args[])
+    {
+
+        // Object of graph is created.
+        Graph<Integer> g = new Graph<Integer>();
+
+        // edges are added.
+        // Since the graph is bidirectional,
+        // so boolean bidirectional is passed as true.
+        g.addEdge(0, 1, true);
+        g.addEdge(0, 4, true);
+        g.addEdge(1, 2, true);
+        g.addEdge(1, 3, true);
+        g.addEdge(1, 4, true);
+        g.addEdge(2, 3, true);
+        g.addEdge(3, 4, true);
+
+        // print the graph.
+        System.out.println("Graph:\n"
+                + g);
+
+        // gives the no of vertices in the graph.
+        g.getVertexCount();
+
+        // gives the no of edges in the graph.
+        g.getEdgesCount(true);
+
+        // tells whether the edge is present or not.
+        g.hasEdge(3, 4);
+
+        // tells whether vertex is present or not
+        g.hasVertex(5);
     }
 }
